d764b699b86fa0003a5cc29f0d637772d63b090d
Antonio Carrillo <Antoniogit config --global user.email Antonio>
2023-02-25 22:56:44 -0600

    User services rev 1 25/02/2023 22:56

---
+++ Services/sicae/src/main/java/mx/edu/utez/sicae/controllers/Student/Dtos/StudentDto.java
@@ -0,0 +1,25 @@
+package mx.edu.utez.sicae.controllers.Student.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mx.edu.utez.sicae.models.asistence.Asistence;
+import mx.edu.utez.sicae.models.group.Group;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class StudentDto {
+    private List<Asistence> asistences;
+    private String password;
+    @NotEmpty(message = "Campo obligatorio")
+    private String id;
+    private String name;
+    private String lastname;
+    private Group group;
+}
---
+++ Services/sicae/src/main/java/mx/edu/utez/sicae/controllers/Student/StudentController.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.sicae.controllers.Student;
+
+public class StudentController {
+}
